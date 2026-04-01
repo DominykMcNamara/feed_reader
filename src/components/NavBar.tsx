2676e619bb11e73c1a6ee524b5845b0ad183121c
DominykMcNamara <dominyksmith@gmail.com>
2026-04-01 00:14:50 -0400

    fix: update NavBar layout for consistent spacing and conditional rendering

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,19 +1,32 @@
+import { useState } from "react";
 import Logo from "./Logo";
 
 function NavBar({ links }: { links: string[] }) {
+  const [signedIn] = useState(false);
   return (
-    <nav className="w-full h-16 flex items-center justify-around">
+    <nav className="w-full h-16 flex items-center px-8 justify-between">
       <Logo />
-      <ul className="flex gap-8">
-        {links.map((link, index) => (
-          <a
-            href="#"
-            className='hover:text-primary hover:underline hover:cursor-pointer active:text-primary active:underline'
-          >
-            <li key={index}>{link}</li>
-          </a>
-        ))}
-      </ul>
+      {signedIn ? (
+        <ul className="flex gap-8">
+          {links.map((link, index) => (
+            <a
+              href="#"
+              className="hover:text-primary hover:underline hover:cursor-pointer active:text-primary active:underline"
+            >
+              <li key={index}>{link}</li>
+            </a>
+          ))}
+        </ul>
+      ) : (
+        <a
+          href="#"
+         
+        >
+          <button className=" bg-primary text-white hover:cursor-pointer hover:bg-blue-600 rounded px-4 py-2">
+          Sign In
+          </button>
+        </a>
+      )}
     </nav>
   );
 }
