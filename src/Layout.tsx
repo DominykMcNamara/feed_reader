1627d67548b107e7776c435a2e217319cc334153
DominykMcNamara <dominyksmith@gmail.com>
2026-04-01 23:56:30 -0400

    refactor: update Layout component structure for improved readability

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -3,13 +3,17 @@ import NavBar from "./components/NavBar";
 import Footer from "./components/Footer";
 
 function Layout({ children }: { children: React.ReactNode }) {
-    return (
-        <div className="min-h-screen min-w-screen bg-white font-black">
-            <NavBar links={["Feed", "Digest"]} />
-            {children}
-            <Footer />
-        </div>
-    )
+  return (
+    <>
+      <NavBar links={["Feed", "Digest"]} />
+
+      <div className="min-h-screen min-w-screen bg-white font-black">
+        {children}
+      </div>
+
+      <Footer />
+    </>
+  );
 }
 
 export default Layout;
