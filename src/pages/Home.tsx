b7343fa7dfd4f0a292b864267c7424738408a936
DominykMcNamara <dominyksmith@gmail.com>
2026-04-02 14:56:12 -0400

    refactor: restructure Home component for improved layout and readability

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,10 @@
 function Home() {
   return (
     <div className="w-full h-full">
-
+      <div className="flex flex-row justify-around items-center w-full h-full">
+        <h1 className="text-4xl font-bold text-center">Your front page news where ever you are</h1>
+        <img src="/src/assets/front_page_of_front_page_news.png" alt="Front Page News" className="" />
+      </div>
     </div>
   );
 }
