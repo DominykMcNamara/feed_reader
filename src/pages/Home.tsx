4afa5cfc72937283813fdba8a7068f1a01daf369
DominykMcNamara <dominyksmith@gmail.com>
2026-04-07 00:45:15 -0400

    refactor: update Home component structure for improved layout and readability

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,23 @@
 function Home() {
   return (
-    <div className="w-full h-full">
-      <div className="flex flex-row justify-around items-center w-full h-full">
-        <h1 className="text-4xl font-bold text-center">Your front page news where ever you are</h1>
-        <img src="/src/assets/front_page_of_front_page_news.png" alt="Front Page News" className="" />
+    <section className="w-full h-full">
+      <div className="flex flex-row gap-2 justify-around items-center ">
+        <h1 className="text-4xl font-bold text-center">
+          Your front page news where ever you are
+        </h1>
+        <img
+          src="/src/assets/front_page_of_front_page_news.png"
+          alt="Front Page News"
+          className=""
+        />
       </div>
-    </div>
+      <div className="flex flex-row ">
+        <a href="#">
+          Log in
+        </a>
+        <a>Try as a guest</a>
+      </div>
+    </section>
   );
 }
 
