bb2392fb67306379dd4e2512bd9a8e5553cfc08a
DominykMcNamara <dominyksmith@gmail.com>
2026-04-13 00:48:05 -0400

    fix: wrap image in a div for better layout structure

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,11 +5,13 @@ function Home() {
         <h1 className="text-4xl font-bold text-center">
           Your front page news where ever you are
         </h1>
+        <div className="w-1/2">
         <img
           src="/src/assets/front_page_of_front_page_news.png"
           alt="Front Page News"
-          className=""
+          className="w-full"
         />
+        </div>
       </div>
       <div className="flex flex-row justify-around items-center mt-8">
         <a href="#" className="button">
