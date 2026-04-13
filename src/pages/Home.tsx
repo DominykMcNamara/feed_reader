ee06db5831c12140742447c209ebfd82605d4361
DominykMcNamara <dominyksmith@gmail.com>
2026-04-13 00:59:22 -0400

    fix: adjust layout structure for improved readability in Home component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,25 +1,33 @@
 function Home() {
   return (
-    <section className="w-full h-full">
-      <div className="flex flex-row gap-2 justify-around items-center ">
-        <h1 className="text-4xl font-bold text-center">
-          Your front page news where ever you are
-        </h1>
-        <div className="w-1/2">
-        <img
-          src="/src/assets/front_page_of_front_page_news.png"
-          alt="Front Page News"
-          className="w-full"
-        />
+    <>
+      <section className="w-full h-full">
+        <div className="flex flex-row gap-2 justify-around items-center ">
+          <h1 className="text-4xl font-bold text-center">
+            Your front page news where ever you are
+          </h1>
+          <div className="w-1/2">
+            <img
+              src="/src/assets/front_page_of_front_page_news.png"
+              alt="Front Page News"
+              className="w-full"
+            />
+          </div>
         </div>
-      </div>
-      <div className="flex flex-row justify-around items-center mt-8">
-        <a href="#" className="button">
-          Log in
-        </a>
-        <a className="button">Try as a guest</a>
-      </div>
-    </section>
+        <div className="flex flex-row justify-center gap-96 items-center mt-8">
+          <a href="#" className="button">
+            Log in
+          </a>
+          <a className="button">Try as a guest</a>
+        </div>
+      </section>
+      <section className="w-full h-full mt-16"> 
+        <div className="flex flex-row gap-2 justify-around items-center ">
+          <h1 className="text-4xl font-bold text-center">Features</h1>
+        </div>
+
+      </section>
+    </>
   );
 }
 
